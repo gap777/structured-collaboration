5f5e1ebcd9489a5edce15db31135141a79aa05c6
Stephen Passero <stephen.passero@gmail.com>
2018-04-14 19:41:41 -0400

    Refactor try catch block

--- client/src/Header.js
+++ client/src/Header.js
@@ -2,10 +2,21 @@ import React, { Component } from 'react';
 import './css/Header.css'
 
 class Header extends Component {
+
+    async getParticipants(){
+        const response = await fetch(
+            "/api/meeting/:meetingId/participants",
+            {
+                method: "GET"
+            });
+        return response;
+    }
+
     render() {
         return (
             <div className="header">
                 <p className="sessionNumber">Session# {this.props.sessionNumber}</p>
+                <p className="participantNumber">{this.getParticipants()}</p>
             </div>
         );
     }
--- client/src/LandingPage.js
+++ client/src/LandingPage.js
@@ -17,13 +17,18 @@ class LandingPage extends Component {
   }
 
   async startMeeting() {
-    const response = await fetch(
-      "/api/meeting",
-      {
-        method: "POST"
-      });
-    const json = await response.json();
-    this.props.history.push(`/${json.meetingId}/facilitate`);
+      try {
+          const json = await response.json();
+          const response = await fetch(
+              "/api/meeting",
+              {
+                  method: "POST"
+              });
+          this.props.history.push(`/${json.meetingId}/facilitate`);
+      }  catch(err){
+          alert("There is currently an error with the server. We're sorry for your inconvenience.");
+          return;
+      }
   }
 
   async joinMeeting() {
