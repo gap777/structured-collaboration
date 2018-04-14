797505fbb74977a7adea4e923b1ddfca483d6665
gap <gary.passero@rolemodelsoftware.com>
2018-04-14 14:13:37 -0400

    Create meeting view

---
+++ src/MeetingView.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './MeetingView.css';
+
+class MeetingView extends Component {
+  render() {
+    return (
+      <div/>
+    );
+  }
+}
+
+export default MeetingView;
