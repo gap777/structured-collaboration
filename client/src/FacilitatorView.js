70a9e2bac9eef468236eb43adaeea258e3ff20ec
William <williamwells555@gmail.com>
2018-04-14 19:09:24 -0400

    Facilitator static cards & styling
    Create basic HTML elements and styling of questions on the facilitator page.

--- client/src/FacilitatorView.js
+++ client/src/FacilitatorView.js
@@ -6,7 +6,28 @@ class FacilitatorView extends Component {
     return (
       <React.Fragment>
           <Header sessionNumber={this.props.match.params.meetingId}/>
-          <p>Do you have a question</p>
+            
+            <div className="questionList">    
+                
+                <button className="btn">+ Add a Question</button>
+                
+                <div className="card question"> 
+                    <input type='text' placeholder="Type your question" />
+                    <div className="questionActions">
+                        <button className="btn" >Submit</button>
+                    </div>
+                </div>
+        
+                <div className="card question"> 
+                    <div className="questionTitle">          
+                        <h1>What are your biggest pain points related to this project?</h1>
+                    </div>
+                    <div className="questionActions">
+                        <button className="btn" >End</button>
+                    </div>
+                </div>
+        
+            </div>
       </React.Fragment>
     );
   }
