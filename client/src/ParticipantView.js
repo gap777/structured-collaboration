5e3dff9e9c5a5e3a4b8a14b4adddcd748e9f27aa
Christy Presler <cpresler@users.noreply.github.com>
2018-04-14 21:02:32 -0400

    ADD RESPONSE VIEW TO PARTICIPANT ================================ Add static layout for the participant to respond to questions.

--- client/src/ParticipantView.js
+++ client/src/ParticipantView.js
@@ -7,14 +7,29 @@ class ParticipantView extends Component {
   render() {
     const participantIdString = sessionStorage.getItem('participantId');
     if (!participantIdString) {
-      return <Redirect to='/'/>
+      // return <Redirect to='/'/>
     }
 
     const participantId = parseInt(participantIdString, 10);
     return (
       <React.Fragment>
         <Header sessionNumber={this.props.match.params.meetingId}/>
+        {/* No Question Present*/}
         <p>Participant {participantId}, please wait for a question....</p>
+
+        {/* Question Present*/}
+        <div className="card question">
+          <div className="questionTitle">
+            <h1>What are your biggest pain points related to this project?</h1>
+          </div>
+          <input type='text' placeholder="Type your answer" />
+          <div className="questionActions">
+            <button className="btn" >Submit</button>
+          </div>
+        </div>
+
+
+
       </React.Fragment>
     );
   }
