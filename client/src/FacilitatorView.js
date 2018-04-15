068cef12d8e298f3389387ace86aa33cf6a6042f
Stephen Passero <stephen.passero@gmail.com>
2018-04-15 01:49:18 -0400

    Removed Duplicate Question Block

--- client/src/FacilitatorView.js
+++ client/src/FacilitatorView.js
@@ -52,13 +52,6 @@ class FacilitatorView extends Component {
                                questionMode={this.state.textOrQuestion}/>
 
                 <div className="card question">
-                    <input type='text' placeholder="Type your question" />
-                    <div className="questionActions">
-                        <button className="btn" >Submit</button>
-                    </div>
-                </div>
-
-                <div className="card question">
                     <div className="questionTitle">
                         <h1>What are your biggest pain points related to this project?</h1>
                     </div>
