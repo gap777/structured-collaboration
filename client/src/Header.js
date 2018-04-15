d5fe16d7d0db53b69d9e8e728e1cfaa4ed45e3a6
gap <gary.passero@rolemodelsoftware.com>
2018-04-14 20:28:09 -0400

    Fix header rendering error with participant count

--- client/src/Header.js
+++ client/src/Header.js
@@ -3,23 +3,38 @@ import './css/Header.css'
 
 class Header extends Component {
 
-    async getParticipants(){
-        const response = await fetch(
-            "/api/meeting/:meetingId/participants",
-            {
-                method: "GET"
-            });
-        return response;
+  constructor(props) {
+    super(props);
+    this.state = {
+      numberParticipants: undefined
     }
+  }
 
-    render() {
-        return (
-            <div className="header">
-                <p className="sessionNumber">Session# {this.props.sessionNumber}</p>
-                <p className="participantNumber">{this.getParticipants()}</p>
-            </div>
-        );
-    }
+  componentWillMount() {
+    this.getParticipants()
+        .then(participantCount => this.setState({
+          numberParticipants: participantCount
+        }));
+  }
+
+  async getParticipants(){
+    const response = await fetch(
+      "/api/meeting/:meetingId/participants",
+      {
+        method: "GET"
+      });
+    const json = response.json();
+    return json.participants;
+  }
+
+  render() {
+    return (
+        <div className="header">
+            <p className="sessionNumber">Session# {this.props.sessionNumber}</p>
+            <p className="participantNumber">{this.state.numberParticipants || 0}</p>
+        </div>
+    );
+  }
 }
 
 export default Header;
